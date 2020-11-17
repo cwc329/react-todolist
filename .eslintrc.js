cd771d234b943cdfcb55d9b714c18f0b137a8874
cwc329 <iaiminwtg@gmail.com>
2020-11-18 04:09:18 +0800

    revise eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'no-unused-vars': 'off',
+    'consistent-return': 'off',
+    'no-undef': 'off',
+  },
+};
